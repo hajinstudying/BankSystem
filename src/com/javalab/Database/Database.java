02bbf197b7b2831cfcf1acf747f21379ffb58a79
hajinstudying <studyinghajin2@gmail.com>
2024-04-25 13:12:20 +0900

    second commit

--- src/com/javalab/Database/Database.java
+++ src/com/javalab/Database/Database.java
@@ -1,18 +1,34 @@
 package com.javalab.Database;
 
+import com.javalab.domain.Account;
+import com.javalab.domain.Bank;
 import com.javalab.domain.Customer;
+import com.javalab.domain.Transaction;
 
 import java.util.ArrayList;
 
 public class Database {
-    public static void main(String[] args) {
-        ArrayList<Customer> customers = new ArrayList<>();
-        //id/이름/주민번호/계좌/잔액/비밀번호
-        customers.add(new Customer(1,"홍길동",900101,3560598023693,50000000,1234));
-        customers.add(new Customer(2,"김길동",900101,5468465454654,50000000,1234));
-        customers.add(new Customer(3,"이길동",900101,8464518951384,50000000,1234));
-        customers.add(new Customer(4,"서길동",900101,8489465123154,50000000,1234));
-        customers.add(new Customer(5,"윤길동",900101,3030303,50000000,1234));
-        customers.add(new Customer(6,"심길동",900101,3030303,50000000,1234));
+    //고객 클래스 리스트
+    ArrayList<Customer> customers = new ArrayList<>();
+    //계좌 클래스 리스트
+    ArrayList<Account> accounts = new ArrayList<>();
+    //거래내역 클래스 리스트
+    ArrayList<Transaction> transactions = new ArrayList<>();
+    //은행 클래스 리스트
+    ArrayList<Bank> banks = new ArrayList<>();
+
+    //기본 생성자
+    public Database(){
+        initializeData();   //데이터 생성작업을 별도의 메소드로 분리
     }
+
+    //데이터 생성 메소드
+    public void initializeData(){
+        //고객 리스트 초기화
+
+        //계좌 리스트 초기화
+        //거래내역 리스트 초기화
+        //은행 리스트 초기화
+    }
+
 }
--- src/com/javalab/domain/Customer.java
+++ src/com/javalab/domain/Customer.java
@@ -1,12 +1,44 @@
 package com.javalab.domain;
 
 public class Customer {
-    //field
+    //회원Id, 이름, 주민번호, 비밀번호
     private int Id;         //회원ID
     private String name;    //이름
-    private int ssn;        //주민번호
-    private int account;    //계좌
-    private int balance;    //잔액
     private int pw;         //비밀번호
 
+    //기본생성자
+    public Customer() {
+    }
+    //오버로딩 생성자
+    public Customer(int id, String name, Account acount, int pw) {
+        Id = id;
+        this.name = name;
+        this.pw = pw;
+    }
+
+    //게터 세터
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPw() {
+        return pw;
+    }
+
+    public void setPw(int pw) {
+        this.pw = pw;
+    }
 }
